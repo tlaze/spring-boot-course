b450315282c2392ca035a715884912c126232397
Tom <tomlazore@gmail.com>
2025-10-23 15:02:53 -0400

    query students by last name

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -20,9 +22,12 @@ public class CruddemoApplication {
         return runner -> {
 //            createStudent(studentDAO);
 //            createMultipleStudents(studentDAO);
-            readStudent(studentDAO);
+//            readStudent(studentDAO);
+//            queryForStudents(studentDAO);
+            queryForStudentsByLastName(studentDAO);
         };
     }
+
 
     private void createStudent(StudentDAO studentDAO) {
 
@@ -72,5 +77,26 @@ public class CruddemoApplication {
 
         // Display student
         System.out.println("Found the student: " + myStudent);
+    }
+
+    private void queryForStudents(StudentDAO studentDAO) {
+
+        // Get a list of students
+        List<Student> theStudents = studentDAO.findAll();
+
+        // Display list of students
+        for(Student tempStudent: theStudents){
+            System.out.println(tempStudent);
+        }
+    }
+
+    private void queryForStudentsByLastName(StudentDAO studentDAO) {
+        // Get  a list of students
+        List<Student> theStudents = studentDAO.findByLastName("Doe");
+
+        // Display list of students
+        for(Student tempStudent : theStudents){
+            System.out.println(tempStudent);
+        }
     }
 }
--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -2,9 +2,12 @@ package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Student;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO{
@@ -31,6 +34,27 @@ public class StudentDAOImpl implements StudentDAO{
     @Override
     public Student findById(Integer Id) {
         return entityManager.find(Student.class, Id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        // Create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+        // Return query results
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+        // Create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student WHERE lastName=:theData", Student.class);
+
+        // Set query parameters
+        theQuery.setParameter("theData", theLastName);
+
+        // Return query results
+        return theQuery.getResultList();
     }
 
 
