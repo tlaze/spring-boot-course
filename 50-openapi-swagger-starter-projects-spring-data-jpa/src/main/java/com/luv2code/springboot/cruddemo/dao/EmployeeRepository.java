7aa091cda69b45bdc0c5cd7f27cf609f1f8e79d4
Tom <tomlazore@gmail.com>
2025-11-04 10:23:13 -0500

    used api docs dependency to test rest api with swagger

--- 50-openapi-swagger-starter-projects-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ 50-openapi-swagger-starter-projects-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,10 +2,9 @@ package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-    // That's it. not need to write any code!
+    // that's it ... no need to write any code LOL!
+
 }
