0d508fb75497daf7babf966490fe19578a7a28a4
Tom <tomlazore@gmail.com>
2025-11-04 11:27:49 -0500

    added authorization to different employees based on ROLE

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,9 +2,15 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasRole;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -13,9 +19,30 @@ public class DemoSecurityConfig {
     public InMemoryUserDetailsManager userDetailsManager(){
 
         UserDetails john = User.builder().username("john").password("{noop}test123").roles("EMPLOYEE").build();
-        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE").build();
-        UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE").build();
+        UserDetails mary = User.builder().username("mary").password("{noop}test123").roles("EMPLOYEE","MANAGER").build();
+        UserDetails susan = User.builder().username("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
 
         return new InMemoryUserDetailsManager(john,mary,susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                .requestMatchers(HttpMethod.GET, "/api/employees/").hasRole("EMPLOYEE")
+                                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                                .requestMatchers(HttpMethod.POST, "/api/employees/").hasRole("MANAGER")
+                                .requestMatchers(HttpMethod.PUT, "/api/employees/").hasRole("MANAGER")
+                                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        // use HTTP basic Authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request Forgery (CRSF)
+        // In general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
