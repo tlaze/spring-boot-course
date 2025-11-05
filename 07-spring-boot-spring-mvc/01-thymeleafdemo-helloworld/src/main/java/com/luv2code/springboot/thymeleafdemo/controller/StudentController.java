50685d5f3704ac7d0c475cd1de4d437d32b05022
Tom <tomlazore@gmail.com>
2025-11-05 13:56:52 -0500

    added confirmation html page.

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/StudentController.java
@@ -3,12 +3,14 @@ package com.luv2code.springboot.thymeleafdemo.controller;
 import com.luv2code.springboot.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
 
+    @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
 
         // Create a student object
