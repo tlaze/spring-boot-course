d05b8ed70ec8e67a44346b739b42a78b2ca154d6
Tom <tomlazore@gmail.com>
2025-11-05 13:19:24 -0500

    created a new verison of processForm request to capitalize the students name

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +17,25 @@ public class HelloWorldController {
     // Need a controller method to process the HTML form
     @RequestMapping("processForm")
     public String processForm(){
+        return "helloworld";
+    }
+
+    // Need a controller method to read form data and add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model){
+
+        // Read the request parameter from HTML form
+        String theName = request.getParameter("studentName");
+
+        // Convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // Create the message
+        String result = "Yo! " + theName;
+
+        // Add message to the model
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 }
