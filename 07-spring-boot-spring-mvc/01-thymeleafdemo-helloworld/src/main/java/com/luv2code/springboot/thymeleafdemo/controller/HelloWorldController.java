650238b232fe232b9d621e3ad7b9a393d5af21d7
Tom <tomlazore@gmail.com>
2025-11-05 13:04:30 -0500

    created a rest controller for a form and html template for typing student's names

---
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    // Need a controller method to show initial HTML form
+    @RequestMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    // Need a controller method to process the HTML form
+}
