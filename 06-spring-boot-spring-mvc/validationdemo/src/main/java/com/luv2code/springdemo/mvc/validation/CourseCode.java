15d435ccc37ba97c872aa6e15ac1fd13e26a7a0f
Tom <tomlazore@gmail.com>
2025-11-14 14:06:00 -0500

    created annotation class for valdition

---
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/validation/CourseCode.java
@@ -0,0 +1,14 @@
+package com.luv2code.springdemo.mvc.validation;
+
+import jakarta.validation.Constraint;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = CourseCodeConstraintValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CourseCode {
+}
