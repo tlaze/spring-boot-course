9e567d7f5d12792fb17ef893775f1a5c36bb58c0
Tom <tomlazore@gmail.com>
2025-11-14 13:14:02 -0500

    added println for debugging error codes with binding results

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -36,6 +36,10 @@ public class CustomerController {
 
         System.out.println("Last name: |" + theCustomer.getLastName() + "|");
 
+        System.out.println("Binding results: " + theBindingResult.toString());
+
+        System.out.println("\n\n\n\n\n");
+
         if(theBindingResult.hasErrors()){
             return "customer-form";
         }
