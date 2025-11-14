23d36eee5ef0fc90aeac1e655a226b1e1e781c81
Tom <tomlazore@gmail.com>
2025-11-14 13:02:23 -0500

    added validation using regex for 5 chars/digits for postal code

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,9 +1,6 @@
 package com.luv2code.springdemo.mvc;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -13,10 +10,21 @@ public class Customer {
     @Size(min=1, message="is required")
     private String lastName = "";
 
-    private int freePasses;
-
     @Min(value=0, message="must be greater than or equal to zero")
     @Max(value=10, message="must be lesser than or equal to 10")
+    private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
     public int getFreePasses() {
         return freePasses;
     }
